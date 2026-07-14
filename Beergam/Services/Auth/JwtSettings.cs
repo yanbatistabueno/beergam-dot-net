ae28c1e8c866438d4c6411decfdfb09c114768c1
Yan Batista <yanbatista2704@gmail.com>
2026-07-14 15:29:19 -0300

    feat(Auth): adding setters to jwt lol

--- Beergam/Services/Auth/JwtSettings.cs
+++ Beergam/Services/Auth/JwtSettings.cs
@@ -2,8 +2,8 @@ namespace Beergam.Services.Auth;
 
 public class JwtSettings
 {
-    public string Issuer { get; } = string.Empty;
-    public string Audience { get; } = string.Empty;
-    public string SecretKey { get; } = string.Empty;
-    public int ExpirationMinutes { get; } = 10;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public string SecretKey { get; set; } = string.Empty;
+    public int ExpirationMinutes { get; set; } = 10;
 }
